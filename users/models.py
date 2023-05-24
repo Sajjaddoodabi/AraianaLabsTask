876bccdb755079811f76f119e25c68fb01f96bec
Sajjaddoodabi <sajjaddoodabi55@gamil.com>
2023-05-24 11:07:28 +0330

    add user app -add user model

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+def get_profile_image(self, file_name):
+    return f'images/profile_images/{self.username}.jpg'
+
+
+class User(AbstractUser):
+    profile_image = models.ImageField(
+        upload_to=get_profile_image,
+        max_length=255,
+        default='images/profile_images/Default.png'
+    )
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.username
+
