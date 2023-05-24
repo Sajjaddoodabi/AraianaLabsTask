09486f28911d8223dbab60f6e02e56220be35ff5
Sajjaddoodabi <sajjaddoodabi55@gamil.com>
2023-05-24 11:15:43 +0330

    add validate password and create function to register serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from users.models import User
@@ -23,3 +24,32 @@ class UserRegister(serializers.ModelSerializer):
         extra_kwargs = {
             'email': {'required': True}
         }
+
+    def validate(self, attrs):
+        # check if password is strong enough
+        validate_password(attrs.get('password'))
+
+        # checking if password and confirm password are the same
+        if attrs.get('password') != attrs.get('confirm_password'):
+            raise serializers.ValidationError(_('password and confirm password does NOT match!'))
+
+        return attrs
+
+    @staticmethod
+    def clean_validated_data(validated_data):
+        # deleting confirm password because we don't need it anymore
+        validated_data.pop('confirm_password')
+
+        return validated_data
+
+    def create(self, validated_data):
+        # create user with given data
+        user = self.Meta.model.objects.create(**self.clean_validated_data(validated_data))
+
+        # set password for user
+        user.set_password(validated_data.get('password'))
+
+        # save user into database
+        user.save()
+
+        return user
